66377d9943bb184f35180614a1fc06d61beb63d1
Shreyansh2610 <shreyansh7874056628@gmail.com>
2025-12-29 23:17:32 +0530

    docker setup

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM node:18-alpine
+
+# Create app directory
+WORKDIR /app
+
+# Copy package files
+COPY package*.json ./
+
+# Install dependencies
+RUN npm install --production
+
+# Copy source code
+COPY . .
+
+# Expose application port
+EXPOSE 3000
+
+# Start application
+CMD ["node", "server.js"]
--- scripts/createAdmin.js
+++ scripts/createAdmin.js
@@ -3,9 +3,10 @@ const User = require("../models/User");
 
 const createAdmin = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://nest4us_user:Zvgi0EmOSKdEY6DD@cluster0.nxavpeg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    );
+    // await mongoose.connect(
+    //   "mongodb+srv://nest4us_user:Zvgi0EmOSKdEY6DD@cluster0.nxavpeg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    // );
+    await mongoose.connect(process.env.MONGO_URI);
 
     const adminExists = await User.findOne({ role: "admin" });
     if (adminExists) {
